884d82c39b4969d00593bf4217332fd14ae2eda1
kaien <kaienkira@gmail.com>
2017-08-14 10:43:31 +0800

    move random class mt19937 implemation from brickred-server lib to brickred-codec lib

--- mak/test/testrandom.mak
+++ mak/test/testrandom.mak
@@ -2,7 +2,7 @@ TARGET = bin/testrandom
 SRCS = src/test/test_random.cc
 LINK_TYPE = exec
 INCLUDE = -Isrc
-LIB = -Lbuild -lbrickredserver -lbrickredcore -lbrtest -pthread -lrt
+LIB = -Lbuild -lbrickredserver -lbrickredcodec -lbrickredcore -lbrtest -pthread -lrt
 DEPFILE = build/libbrickredserver.a build/libbrtest.a
 BUILD_DIR = build
 
--- src/brickred/random.h
+++ src/brickred/random.h
@@ -5,34 +5,19 @@
 #include <cstddef>
 
 #include <brickred/class_util.h>
-#include <brickred/unique_ptr.h>
+#include <brickred/codec/mt19937.h>
 
 namespace brickred {
 
-class Random {
+class Random : public codec::Mt19937 {
 public:
     Random();
     Random(uint32_t s);
     Random(uint32_t key[], size_t key_length);
-    ~Random();
-
-    void seed(uint32_t s);
-    void seed(uint32_t key[], size_t key_length);
-
-    // uniformly distributed on range [0, 0xffffffff]
-    uint32_t nextInt();
-    // uniformly distributed on range [0, max)
-    uint32_t nextInt(uint32_t max);
-    // uniformly distributed on range [min, max]
-    uint32_t nextInt(uint32_t min, uint32_t max);
-    // uniformly distributed on range [0, 1)
-    double nextDouble();
+    virtual ~Random();
 
 private:
     BRICKRED_NONCOPYABLE(Random)
-
-    class Impl;
-    UniquePtr<Impl> pimpl_;
 };
 
 } // namespace brickred
